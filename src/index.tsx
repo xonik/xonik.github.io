5fd91e04f5c7f67cc630eff7b2cd629f0cbfecf8
xonik <xonik@lednet.org>
2019-12-22 21:51:12 +0100

    Updates

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import RouterComponent from './router/RouterComponent';
-import './index.scss';
-
-ReactDOM.render(
-  <RouterComponent />,
-  document.getElementById('root')
-);
--- src/layout/Header.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './Header.scss';
-
-export default () => {
-  return <header className="header"></header>
-}
