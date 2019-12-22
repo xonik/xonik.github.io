1c37dde9b4f62d7cc8f17e735eba0a651f438e55
xonik <xonik@lednet.org>
2019-12-22 20:46:47 +0100

    Updates

---
+++ bootloader/precache-manifest.f0f59973da80f453ab6c9f65ce49a7ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b308a477da2cbf3f105",
+    "url": "/bootloader/static/js/runtime~main.1b308a47.js"
+  },
+  {
+    "revision": "b080bbbfee8f7f8dbe2a",
+    "url": "/bootloader/static/js/main.b080bbbf.chunk.js"
+  },
+  {
+    "revision": "f4ac8a0bc2c99297e489",
+    "url": "/bootloader/static/js/1.f4ac8a0b.chunk.js"
+  },
+  {
+    "revision": "b080bbbfee8f7f8dbe2a",
+    "url": "/bootloader/static/css/main.d5c2c0f6.chunk.css"
+  },
+  {
+    "revision": "e1bf126d0c0142af4c6b7a71745967d7",
+    "url": "/bootloader/index.html"
+  }
+];
---
+++ theory/theory.js
@@ -0,0 +1,5 @@
+function openDiv(event) {
+  console.log('here')
+  event.preventDefault();
+  alert("hi");
+}
