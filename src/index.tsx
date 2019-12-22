44a7566726555d91ea0a7286c00e3a4d25d57df9
xonik <xonik@lednet.org>
2019-12-22 21:34:03 +0100

    Removed service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
 import RouterComponent from './router/RouterComponent';
 import './index.scss';
 
