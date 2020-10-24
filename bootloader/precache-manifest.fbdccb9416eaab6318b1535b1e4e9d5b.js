740e081af9d00e3f0ea4104dba6667498af38ab3
xonik <xonik@lednet.org>
2020-10-24 20:14:32 +0200

    Updates

---
+++ bootloader/precache-manifest.fbdccb9416eaab6318b1535b1e4e9d5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fee0ce719ef89ec0cec91e4bef6244b",
+    "url": "/bootloader/index.html"
+  },
+  {
+    "revision": "24630ae1b67825e3e813",
+    "url": "/bootloader/static/css/main.b1ec85e8.chunk.css"
+  },
+  {
+    "revision": "de4c2e64c98a54adeeae",
+    "url": "/bootloader/static/js/2.99ba9dec.chunk.js"
+  },
+  {
+    "revision": "24630ae1b67825e3e813",
+    "url": "/bootloader/static/js/main.8a1d6d5d.chunk.js"
+  },
+  {
+    "revision": "0a0bc908c4b929c41467",
+    "url": "/bootloader/static/js/runtime-main.5dcd3c22.js"
+  }
+]);
